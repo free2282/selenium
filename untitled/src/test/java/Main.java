e8bcce99cd2c93b90bab3a3329e7bdff2708953e
Bogdanovich Dmitry <free2282@ya.ru>
2023-09-02 20:01:43 +0300

    firstPartOfProject

---
+++ untitled/src/test/java/Main.java
@@ -0,0 +1,13 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        WebDriverManager.chromedriver().setup();
+
+    }
+
+}
---
+++ untitled/src/test/java/WebTest.java
@@ -0,0 +1,71 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.*;
+import org.junit.Test;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.assertEquals;
+
+public class WebTest
+{
+    private WebDriver driver;
+
+
+
+
+    @Test
+    public void clickButtonEnter()
+    {
+
+        driver.findElement(By.xpath(".//button[@class='auth-form__button']")).click();
+    }
+    @Test
+    public void searchElement()
+    {
+        driver.findElement(By.xpath(".//h3[@class='auth-form__title']"));
+    }
+    @Test
+    public void logInCheckExit()
+    {
+        driver.findElement(By.xpath(".//button[@class='header__logout']"));
+        String exit = driver.findElement(By.xpath(".//button[@class='header__logout']")).getText();
+        System.out.println(exit);
+    }
+    @Test
+    public void logInCheckFooter()
+    {
+        
+        driver.findElement(By.className("footer__copyright"));
+        WebElement element = driver.findElement(By.className("footer__copyright"));
+        ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView();", element);
+        String footer = driver.findElement(By.className("footer__copyright")).getText();
+        System.out.println(footer);
+    }
+
+
+    @Before
+    public void startUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.get("https://qa-mesto.praktikum-services.ru/");
+        String mail = "free2282@ya.ru";
+        String password = "Freein22820";
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        driver.findElement(By.xpath(".//input[@id='email']")).sendKeys(mail);
+        driver.findElement(By.xpath(".//input[@id='password']")).sendKeys(password);
+        driver.findElement(By.xpath(".//button[@class='auth-form__button']")).click();
+    }
+    @After
+    public void tearDown()
+    {
+        driver.quit();
+    }
+}
